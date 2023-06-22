53f09bb3bb84b306c343c37212226e77805570e7
Guilherme Santos Soares <guilherme.soares@equipeblue.com.br>
2023-06-22 17:07:10 -0300

    Primeiro Commit

---
+++ src/main/java/com/nelioalves/workshomongo/WorkshomongoApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.workshomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshomongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/workshomongo/WorkshomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.workshomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
